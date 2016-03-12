469b61ecaff89c405559993e6456686336977ef6
wilsongau <ws_gau@yahoo.com>
2016-03-11 17:21:18 -0800

    Added files via upload

--- MovieStore/MovieStore/FileReader.cpp
+++ MovieStore/MovieStore/FileReader.cpp
@@ -3,3 +3,34 @@
 FileReader::FileReader()
 {
 }
+
+FileReader::FileReader(const string & filename)
+{
+	ifstream infile1(filename);
+	in = &infile1;
+}
+
+FileReader::~FileReader()
+{
+}
+
+bool FileReader::getNextLine(string & result)
+{
+	if ((*in).eof()) 
+	{
+		getline(*in, result);
+		return true;
+	}
+	return false;
+}
+
+bool FileReader::setFileName(const string & filename)
+{
+	ifstream newfile(filename);
+	if (newfile)
+	{
+		in = &newfile;
+		return true;
+	}
+	return false;
+}
